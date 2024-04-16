b210af77745a9012cd614d75820698841a371304
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-16 02:38:21 +0100

    initial test for memory repository

---
+++ internal/repository/memory_repository_test.go
@@ -0,0 +1,38 @@
+package repository_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/m3lnic/think-money-technical-test/internal/repository"
+)
+
+type TestStructPleaseIgnore struct {
+	Data string
+}
+
+func TestMemoryRepositoryCRUDComplexType(t *testing.T) {
+	t.Parallel()
+
+	testKey := "test"
+	testData := "value"
+
+	memoryRepository := repository.NewMemory[string, TestStructPleaseIgnore]()
+
+	// > Create
+	createdData, err := memoryRepository.Create(testKey, TestStructPleaseIgnore{Data: testData})
+	if err != nil {
+		t.Errorf("expected nil, got error(%+v)", err)
+	}
+	if createdData.Data != testData {
+		t.Errorf("expected string(%s), got string(%s)", testData, createdData)
+	}
+
+	_, err = memoryRepository.Create(testKey, TestStructPleaseIgnore{Data: testData})
+	if err == nil {
+		t.Errorf("expected error(%+v), got nil", repository.ErrKeyAlreadyExists)
+	}
+	if !errors.Is(err, repository.ErrKeyAlreadyExists) {
+		t.Errorf("expected error(%+v), got error(%+v)", repository.ErrKeyAlreadyExists, err)
+	}
+}
