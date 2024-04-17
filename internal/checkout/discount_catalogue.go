51b8c037efbab03d994178e013ee86a3441b0217
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-17 21:23:20 +0100

    Implement new create for discount catalogue

--- internal/checkout/discount_catalogue.go
+++ internal/checkout/discount_catalogue.go
@@ -2,8 +2,26 @@ package checkout
 
 import "github.com/m3lnic/think-money-technical-test/internal/repository"
 
-type IDiscountCatalogueRepository repository.IRepository[string, *Discount]
+type IDiscountCatalogueRepository interface {
+	repository.IRepository[string, *Discount]
+}
 
-func NewDiscountCatalogue() IDiscountCatalogueRepository {
-	return repository.NewMemory[string, *Discount]()
+func NewDiscountCatalogue(catalogue ICatalogueRepository) IDiscountCatalogueRepository {
+	return &discountCatalogueRepository{
+		IRepository: repository.NewMemory[string, *Discount](),
+		catalogue:   catalogue,
+	}
+}
+
+type discountCatalogueRepository struct {
+	repository.IRepository[string, *Discount]
+	catalogue ICatalogueRepository
+}
+
+func (dcr *discountCatalogueRepository) Create(key string, data *Discount) (*Discount, error) {
+	if _, err := dcr.catalogue.Read(key); err != nil {
+		return nil, err
+	}
+
+	return dcr.IRepository.Create(key, data)
 }
