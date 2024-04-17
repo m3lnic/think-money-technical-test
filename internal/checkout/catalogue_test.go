f50b99ca56c6e59053c00aecdf794bae9aae1bc8
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-17 19:00:50 +0100

    Update tests to be more readable

--- internal/checkout/catalogue_test.go
+++ internal/checkout/catalogue_test.go
@@ -6,22 +6,31 @@ import (
 	"github.com/m3lnic/think-money-technical-test/internal/checkout"
 )
 
+// > Step by step test
+// > Parent test is parallisable, children tests aren't
 func TestNewCatalogue(t *testing.T) {
+	t.Parallel()
+
 	itemTestName := "Test"
 
 	myCatalogue := checkout.NewCatalogue()
-	newItem := checkout.NewItem(itemTestName, 50)
 
-	_, err := myCatalogue.Create("A", newItem)
-	if err != nil {
-		t.Errorf("expected nil, got error(%+v)", err)
-	}
+	t.Run("creates new item", func(t *testing.T) {
+		if _, err := myCatalogue.Create("A", checkout.NewItem(itemTestName, 50)); err != nil {
+			t.Errorf("expected nil, got error(%+v)", err)
+		}
+		if _, err := myCatalogue.Create("A", checkout.NewItem(itemTestName, 50)); err == nil {
+			t.Errorf("expected err(%+v), got nil", err)
+		}
+	})
 
-	fetchedVal, err := myCatalogue.Read("A")
-	if err != nil {
-		t.Errorf("expected nil, got error(%+v)", err)
-	}
-	if fetchedVal.GetName() != itemTestName {
-		t.Errorf("expected string(%s), got string(%s)", itemTestName, fetchedVal.GetName())
-	}
+	t.Run("reads new item", func(t *testing.T) {
+		fetchedVal, err := myCatalogue.Read("A")
+		if err != nil {
+			t.Errorf("expected nil, got error(%+v)", err)
+		}
+		if fetchedVal.GetName() != itemTestName {
+			t.Errorf("expected string(%s), got string(%s)", itemTestName, fetchedVal.GetName())
+		}
+	})
 }
--- internal/checkout/checkout_test.go
+++ internal/checkout/checkout_test.go
@@ -1,5 +1,72 @@
-package checkout
+package checkout_test
 
-import "testing"
+import (
+	"errors"
+	"testing"
 
-func TestCheckout(t *testing.T) {}
+	"github.com/m3lnic/think-money-technical-test/internal/checkout"
+	"github.com/m3lnic/think-money-technical-test/internal/repository"
+)
+
+// > Step by step test
+func TestCheckoutScan(t *testing.T) {
+	t.Parallel()
+
+	// > Should really use mocking here
+	myCat := checkout.NewCatalogue()
+	pineapple, _ := myCat.Create("A", checkout.NewItem("Pineapples", 50))
+	// waffle, _ := myCat.Create("B", checkout.NewItem("Waffles", 25))
+	// bacon, _ := myCat.Create("C", checkout.NewItem("Bacon", 100))
+
+	myCheckout := checkout.New(myCat)
+
+	t.Run("validate total is initally 0", func(t *testing.T) {
+		if val, _ := myCheckout.GetTotal(); val != 0 {
+			t.Errorf("expected int(0), got int(%d)", val)
+		}
+	})
+
+	t.Run("when sku scanned, total is calculated correctly", func(t *testing.T) {
+		err := myCheckout.Scan("A")
+		if err != nil {
+			t.Errorf("expected nil, got err(%+v)", err)
+		}
+
+		if val, _ := myCheckout.GetTotal(); val != pineapple.GetUnitPrice() {
+			t.Errorf("expected int(%d), got int(%d)", pineapple.GetUnitPrice(), val)
+		}
+
+		err = myCheckout.Scan("A")
+		if err != nil {
+			t.Errorf("expected nil, got err(%+v)", err)
+		}
+
+		if val, _ := myCheckout.GetTotal(); val != pineapple.GetUnitPrice()*2 {
+			t.Errorf("expected int(%d), got int(%d)", pineapple.GetUnitPrice()*2, val)
+		}
+	})
+
+	t.Run("errors when sku not found", func(t *testing.T) {
+		err := myCheckout.Scan("B")
+		if err == nil {
+			t.Errorf("expected err(%+v), got nil", repository.ErrKeyNotFound)
+		}
+		if !errors.Is(err, repository.ErrKeyNotFound) {
+			t.Errorf("expected err(%+v), got err(%+v)", repository.ErrKeyNotFound, err)
+		}
+	})
+
+	t.Run("when sku has been removed before getting the total", func(t *testing.T) {
+		myCat.Create("B", checkout.NewItem("Waffles", 25))
+		myCheckout.Scan("B")
+		myCat.Delete("B")
+
+		if _, err := myCheckout.GetTotal(); err != nil {
+			if !errors.Is(err, repository.ErrKeyNotFound) {
+				t.Errorf("expected err(%+v), got err(%+v)", repository.ErrKeyNotFound, err)
+			}
+		} else {
+			t.Errorf("expected err(%+v), got nil", repository.ErrKeyNotFound)
+		}
+	})
+}
