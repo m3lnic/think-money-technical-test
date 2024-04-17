962a704fa465c8b691efd974c0eb35439b4ae54e
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-17 21:05:22 +0100

    Update item catalogue to use item

--- internal/checkout/catalogue.go
+++ internal/checkout/catalogue.go
@@ -2,8 +2,8 @@ package checkout
 
 import "github.com/m3lnic/think-money-technical-test/internal/repository"
 
-type ICatalogueRepository repository.IRepository[string, IItem]
+type ICatalogueRepository repository.IRepository[string, *Item]
 
 func NewCatalogue() ICatalogueRepository {
-	return repository.NewMemory[string, IItem]()
+	return repository.NewMemory[string, *Item]()
 }
