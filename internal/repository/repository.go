5d02e04f2b22db0b0ea62a412c986b55bfa11dab
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-17 19:18:52 +0100

    Update IRepository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 // > Define repositories to be simple generic interfaces
 type IRepository[K comparable, D any] interface {
+	All() map[K]D
 	Create(K, D) (D, error)
 	Read(K) (D, error)
 	Update(K, D) (D, error)
