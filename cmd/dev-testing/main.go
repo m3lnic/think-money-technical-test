c407b08935595ee2e80fe6c8ee140c4f45fff4c4
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-17 21:23:34 +0100

    Implement discount catalogue in dev-testing

--- cmd/dev-testing/main.go
+++ cmd/dev-testing/main.go
@@ -5,8 +5,13 @@ import "github.com/m3lnic/think-money-technical-test/internal/checkout"
 func main() {
 	myCatalogue := checkout.NewCatalogue()
 	myCatalogue.Create("A", checkout.NewItem("Pineapples", 50))
-	myCatalogue.Create("B", checkout.NewItem("Waffles", 25))
-	myCatalogue.Create("C", checkout.NewItem("Bacon", 100))
+	myCatalogue.Create("B", checkout.NewItem("Waffles", 30))
+	myCatalogue.Create("C", checkout.NewItem("Bacon", 20))
+	myCatalogue.Create("D", checkout.NewItem("Maple Syrup", 15))
+
+	myDiscountCatalogue := checkout.NewDiscountCatalogue(myCatalogue)
+	myDiscountCatalogue.Create("A", checkout.NewDiscount(3, 130))
+	myDiscountCatalogue.Create("B", checkout.NewDiscount(2, 45))
 
 	myCheckout := checkout.New(myCatalogue)
 	myCheckout.Scan("A")
