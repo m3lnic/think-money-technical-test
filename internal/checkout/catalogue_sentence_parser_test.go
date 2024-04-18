9e5ad75909a3950eda30cf24477999122632e33b
m3lnic <melody.astrid.nicholls@gmail.com>
2024-04-18 02:36:44 +0100

    Create template catalogue sentence parser test and rename prev test to func

--- internal/checkout/catalogue_sentence_parser_test.go
+++ internal/checkout/catalogue_sentence_parser_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCatalogueSentenceParser(t *testing.T) {
+func TestCatalogueSentenceParserFunc(t *testing.T) {
 	invalidSentence := "This is an invalid sentence"
 	_, _, _, err := checkout.ParseCatalogueItemOrDiscountFromSentence(invalidSentence)
 	assert.ErrorIs(t, err, checkout.ErrInvalidInput)
@@ -32,4 +32,20 @@ func TestCatalogueSentenceParser(t *testing.T) {
 	assert.Equal(t, "Loaves of Bread", name)
 	assert.Equal(t, 60, cost)
 	assert.Equal(t, 90, quantity)
+}
+
+func TestCatalogueSentenceParser(t *testing.T) {
+	catalogue := checkout.NewCatalogue()
+	catalogue.Create("A", checkout.NewItem("Pineapples", 50))
+	catalogue.Create("B", checkout.NewItem("Waffles", 25))
+	catalogue.Create("C", checkout.NewItem("Loaf of Bread", 10))
+
+	discountCatalogue := checkout.NewDiscountCatalogue(catalogue)
+	discountCatalogue.Create("A", checkout.NewDiscount(3, 100))
+
+	catalogueSentenceParser := checkout.NewCatalogueSentenceParser(catalogue, discountCatalogue)
+
+	inputSentence := "Pineapples cost 80, Pineapples are 5 for 200. Waffles cost 100, Loaf of Bread cost 25. 2 Loaf of Bread cost 40."
+	err := catalogueSentenceParser.Parse(inputSentence)
+	assert.Nil(t, err)
 }
